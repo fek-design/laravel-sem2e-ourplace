6cfeed778bf9a50034f1d87400122c209799d7dd
Felix <fkoesgaard@gmail.com>
2025-12-05 14:30:51 +0100

    opsaetning 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+// This loads home.blade.php when someone goes to "/"
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+// This loads about.blade.php when someone goes to "/about"
+Route::get('/about', function () {
+    return view('about');
+});
+
+// This loads contact.blade.php when someone goes to "/contact"
+Route::get('/catalogue', function () {
+    return view('catalogue');
 });
